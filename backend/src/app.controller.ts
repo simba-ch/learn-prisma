015b23911dca07ab4fa98cefed1893bf6e507700
Simba <18721784110@163.com>
2024-10-31 00:13:50 +0800

    验证问题1

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/user')
+  async createUser(@Body('username') username: string) {
+    const res = await this.appService.createUser(username);
+    console.log("🚀 ~ AppController ~ createUser ~ res:", res)
+    return res
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) { }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createUser(username: string) {
+    return this.prisma.user.create({
+      data: {
+        username,
+        password: '123123',
+        isAdmin: false,
+        profile: {
+          create: {
+            sex: 0
+          }
+        }
+      }
+    })
   }
 }
